6f9096bb2b0325f0cadfe1e0b83df562d566990a
Mallemukken <116568594+Mallemukken@users.noreply.github.com>
2022-10-31 16:23:52 +0100

    Added age distribution for exercise 1

--- 01_func_file.py
+++ 01_func_file.py
@@ -15,3 +15,46 @@ Returns
 result_string : string
     reversed complement string with newline every 60 bp
 """
+
+# Age distribution for exercise 1
+def age_distribution(people_dict):
+    """    
+    Function which takes all the data from a dictonary and finds the age distribution.
+    The result is a count dict with age intervals as keys and count as values
+
+    Parameters
+    ----------
+    people_dict : dict
+        CPR numbers as keys and list with age in position [9] as values.
+
+    Returns
+    -------
+    age_dict : dict
+        dict with age interval as keys and count as values
+    """
+    age_dict = {"0-9":0, "10-19":0, "20-29":0, "30-39":0, "40-49":0, "50-59":0, "60-69":0, "70-79":0, "80-89":0, "90-99":0, "100+":0 }
+    for keys in people_dict.keys():
+        age = people_dict[keys][9]
+        if age < 10:
+            age_dict["0-9"] += 1
+        if age >= 10 and age < 20:
+            age_dict["10-19"] += 1
+        if age >= 20 and age < 30:
+            age_dict["20-29"] += 1
+        if age >= 30 and age < 40:
+            age_dict["30-39"] += 1
+        if age >= 40 and age < 50:
+            age_dict["40-49"] += 1
+        if age >= 50 and age < 60:
+            age_dict["50-59"] += 1
+        if age >= 60 and age < 70:
+            age_dict["60-69"] += 1
+        if age >= 70 and age < 80:
+            age_dict["70-79"] += 1
+        if age >= 80 and age < 90:
+            age_dict["80-89"] += 1
+        if age >= 90 and age < 100:
+            age_dict["90-99"] += 1
+        if age >= 100:
+            age_dict["100+"] += 1
+    return age_dict
